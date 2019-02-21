f7bff1931682ce7ec5c7e60f0c4e1bec6bbdb1e8
Michael Wagner <mwagner@localhost.localdomain>
2019-02-21 14:03:29 -0600

    Created skeleton for HTTP Client.

---
+++ Project_1/httpClient.c
@@ -0,0 +1,15 @@
+#include <arpa/inet.h>
+#include "httpShared.h"
+
+int main(int argC, char **argV){
+
+    int clientFD = 0;
+    int status = 0;
+    int long valRead = 0;
+    char buff[BUFFER_SIZE] = {0};
+    struct sockaddr_in servAddr;
+
+    return 0;
+
+}
+
