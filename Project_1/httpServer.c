d6c5defab6020419038867e1bd2eb275e6da4026
Michael Wagner <mwagner@localhost.localdomain>
2019-02-20 19:00:16 -0600

    Added HTTP server file and created subdirectory 'Project_1'.

---
+++ Project_1/httpServer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+int main(int argC, char **argV){
+
+    int serverFD = 0;
+
+    return 0;
+
+}
+
