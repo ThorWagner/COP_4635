ff607e0c596e9b4da10f7d3920d713582df50b47
Michael Wagner <mwagner@localhost.localdomain>
2019-02-21 14:02:48 -0600

    Included libraries that both server and client use.

--- Project_1/httpShared.h
+++ Project_1/httpShared.h
@@ -1,6 +1,13 @@
 #ifndef HTTP_SHARED_H
 #define HTTP_SHARED_H
 
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
+    #include <unistd.h>
+    #include <netinet/in.h>
+    #include <sys/socket.h>
+    
     #define BUFFER_SIZE 4096
     #define HEADER_SIZE 75
     #define DEF_PORT 60032
