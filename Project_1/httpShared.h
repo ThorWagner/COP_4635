b2d1c9816d6918ceb59967ad6d8bfce0b5d3c3b9
Michael Wagner <mwagner@localhost.localdomain>
2019-02-21 01:30:56 -0600

    Initial commit to upload secondary files.

---
+++ Project_1/httpShared.h
@@ -0,0 +1,10 @@
+#ifndef HTTP_SHARED_H
+#define HTTP_SHARED_H
+
+    #define BUFFER_SIZE 4096
+    #define HEADER_SIZE 75
+    #define DEF_PORT 60032
+    #define WAIT_SIZE 10
+
+#endif /* HTTP_SHARED_H */
+
