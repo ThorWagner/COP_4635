3ef08c0f98bb411a7bc0820b81c17e648d1a444b
Michael Wagner <mcw48@students.uwf.edu>
2019-02-24 22:19:03 -0600

    Function prototypes moved from httpServer.c.

--- Project_1/httpShared.h
+++ Project_1/httpShared.h
@@ -16,5 +16,11 @@
     #define WAIT_SIZE 10
     #define MAX_FILESIZE 100000
 
+    void initServer(int *serverFD, struct sockaddr_in *servAddr, int portNum);
+    void setPort(int argC, char **argV, int *portNum);
+    void buildHeader(char *buff, char *header, char *filename, int *filesize);
+    void serverIntercom(int serverFD, struct sockaddr_in *servAddr);
+    void sigintHandler(int sig);
+
 #endif /* HTTP_SHARED_H */
 
