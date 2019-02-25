2d07ddf3364d13559eb40eefdfe0d004506e4a45
Michael Wagner <mcw48@students.uwf.edu>
2019-02-24 19:56:33 -0600

    Added MAX_FILESIZE definition.

--- Project_1/httpShared.h
+++ Project_1/httpShared.h
@@ -8,11 +8,13 @@
     #include <netinet/in.h>
     #include <sys/socket.h>
     
-    #define BUFFER_SIZE 4096
-    #define HEADER_SIZE 75
-    #define DIRECTORY_SIZE 100
+    #define BUFFER_SIZE 1024
+    #define HEADER_SIZE 512
+    #define ARG_SIZE 100
+    #define BLOCK_SIZE 1024
     #define DEF_PORT 60032
     #define WAIT_SIZE 10
+    #define MAX_FILESIZE 100000
 
 #endif /* HTTP_SHARED_H */
 
